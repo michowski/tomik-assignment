9b12911721cdd79a8efb718aaa57b416f76a2b2a
Arkadiusz Michowski <arkadiusz@michowski.com>
2019-07-24 13:26:28 +0200

    Create Searchbar component

---
+++ src/Searchbar.tsx
@@ -0,0 +1,44 @@
+import React, {
+  FC,
+  useState,
+  ChangeEventHandler,
+  useEffect,
+  useCallback,
+  useRef
+} from "react";
+
+export interface Props {
+  onUpdate: (query: string) => void;
+}
+
+const DEBOUNCE_TIME = 500;
+
+const Searchbar: FC<Props> = ({ onUpdate }) => {
+  const [query, setQuery] = useState("tonik");
+  const lastInputTime = useRef();
+
+  // useCallback, so that the same function ref is passed forever to <input/>
+  const onChange: ChangeEventHandler<HTMLInputElement> = useCallback(e => {
+    setQuery(e.currentTarget.value);
+  }, []);
+
+  // Debounce the `onUpdate` callback
+  useEffect(() => {
+    const prevTime = lastInputTime.current;
+
+    // To early to submit a new value
+    if (prevTime != null && new Date().getTime() - prevTime < DEBOUNCE_TIME) {
+      return;
+    }
+
+    onUpdate(query);
+  }, [query]);
+
+  return (
+    <div>
+      <input className="Searchbar" value={query} onChange={onChange} />
+    </div>
+  );
+};
+
+export default Searchbar;
