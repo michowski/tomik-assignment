a0460a7b567bedbad9ee7155ca1d3423a21bf9f2
Arkadiusz Michowski <arkadiusz@michowski.com>
2019-07-25 14:22:09 +0200

    Cache all the results

--- src/RepositorySearch/Container.tsx
+++ src/RepositorySearch/Container.tsx
@@ -1,6 +1,6 @@
 /* The main container responsible for the state and logic of the feature */
 
-import React, { FC, useState, useEffect, useCallback } from "react";
+import React, { FC, useState, useEffect, useCallback, useRef } from "react";
 
 import Repositories from "./Repositories";
 import {
@@ -14,7 +14,7 @@ import { AjaxStatus, SortOrder } from "../types";
 import SearchStatus from "../components/SearchStatus";
 import Searchbar from "../components/Searchbar";
 import StateHistory from "./StateHistory";
-import { queryParamsFromUrl } from "./utils";
+import { queryParamsFromUrl, queryParamsToUrl } from "./utils";
 
 export interface State {
   queryParams: FetchRepositoriesParams;
@@ -23,6 +23,11 @@ export interface State {
   repositories: Repository[];
   totalCount: number;
 }
+
+export type RepositoriesResults = Pick<State, "repositories" | "totalCount">;
+export type RepositoriesCache = {
+  [url: string]: RepositoriesResults | undefined;
+};
 
 const initState = (): State => {
   const initQueryParams = queryParamsFromUrl(window.location.search);
@@ -39,6 +44,11 @@ const initState = (): State => {
 const Container: FC = () => {
   const [state, setState] = useState(initState);
 
+  // Cache the results by URL
+  // This super-dirty-simple implementation will explode at some point because of RAM
+  // I believe the best approach (aside from HTTP caching) is some bullet-proof TTL/queue solution
+  const cache = useRef<RepositoriesCache>({});
+
   const { queryParams, queryInput, status, repositories, totalCount } = state;
 
   // Make an API call anytime the query params change
@@ -47,6 +57,21 @@ const Container: FC = () => {
       return;
     }
 
+    // Check if we have results cached
+    const url = queryParamsToUrl(queryParams);
+    const cacheHit = cache.current[url];
+    if (cacheHit) {
+      const { totalCount, repositories } = cacheHit;
+
+      return setState(state => ({
+        ...state,
+        status: AjaxStatus.Idle,
+        totalCount,
+        repositories
+      }));
+    }
+
+    // No cache entry - just fetch the data from the API
     setState(state => ({
       ...state,
       status: AjaxStatus.Loading
@@ -54,12 +79,17 @@ const Container: FC = () => {
 
     fetchRepositories(queryParams, ITEMS_PER_PAGE)
       .then(({ data }) => {
+        const { total_count: totalCount, items: repositories } = data;
+
         setState(state => ({
           ...state,
-          status: AjaxStatus.Idle,
-          repositories: data.items,
-          totalCount: data.total_count
+          repositories,
+          totalCount,
+          status: AjaxStatus.Idle
         }));
+
+        // Save the results for later
+        cache.current[url] = { totalCount, repositories };
       })
       .catch(() => {
         setState(state => ({
